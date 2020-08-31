e8376876660d5b92b52dfe9ef2dd751f7b9debd9
Jc7 <jhuinac7@gmail.com>
2020-08-30 23:50:43 -0700

    finally decided on my project 1

--- name-app/app.js
+++ name-app/app.js
@@ -1,13 +1,69 @@
 console.log("Hello");
+const randColor = () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  const o = Math.random();
 
-$.ajax({
-  url: "https://www.scorebat.com/video-api/v1/",
-}).then(
-  (data) => {
-    // $("#video").append(data[0].videos[0].embed); //will display the video
-    $("#video").append(data[5].embed);
-    // $("#video").text(data[0].title + data[0].url);
-    console.log(data[5]);
-  },
-  (error) => {}
-);
+  return `rgb(${r},${g},${b},${o})`;
+};
+
+const showMeal = () => {
+  event.preventDefault();
+  $("#mealsContainer").children().remove();
+  const meal = $("#mealInput").val();
+  $.ajax({
+    url: "https://www.themealdb.com/api/json/v1/1/search.php?s=" + meal,
+    type: "GET",
+  }).then(
+    (data) => {
+      console.log(data.meals[0]);
+      // for (let element of data.meals) {
+      //   console.log(element.strMeal);
+      //   console.log(element.strMealThumb);
+      // }
+      console.log(data);
+      for (let meal of data.meals) {
+        const divCont = $("<div>");
+        const mealName = $("<h3>").text(meal.strMeal);
+        const image = $("<img>").attr("src", meal.strMealThumb);
+        divCont.append(image, mealName);
+
+        $("#mealsContainer").append(divCont);
+      }
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+  $("form").trigger("reset");
+};
+
+$(() => {
+  $.ajax({
+    url: "https://www.themealdb.com/api/json/v1/1/categories.php",
+    type: "GET",
+  }).then(
+    (data) => {
+      console.log(data.categories);
+
+      for (let category of data.categories) {
+        if (category.strCategory === "Miscellaneous") {
+          continue;
+        }
+        const div = $("<div>");
+        const categoryName = $("<h3>").text(category.strCategory);
+        console.log(category.strCategory);
+        const image = $("<img>").attr("src", category.strCategoryThumb);
+        const description = $("<p>").text(category.strCategoryDescription);
+        div.append(categoryName, image, description);
+        $("#description-container").append(div);
+      }
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+});
+
+$("#lookMeal").on("click", showMeal);
